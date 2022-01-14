49b7bea393f01b53494cad408e4be4bb0916c97b
Trinidad Sheridan <88852126+trinidades@users.noreply.github.com>
2022-01-14 02:23:21 +0100

    404 update

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -5,6 +5,8 @@ const NotFound = () => (
   <Layout>
     <h1>404 - Page Not Found</h1>
     <p>Unfortunately we couldn't find what you were looking for :(</p>
+    <p>Please try with a different link</p>
+    <p>Or go back to our homepage</p>
   </Layout>
 )
 
